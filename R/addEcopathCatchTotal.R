9aa662cd4d0753ed420dafbb78e6779429af7cb1
Gustav Delius <gustav.delius@gmail.com>
2024-12-12 15:33:10 +0000

    More documentation for `addEcopathCatchTotal()`

--- R/addEcopathCatchTotal.R
+++ R/addEcopathCatchTotal.R
@@ -1,4 +1,12 @@
-#' Add Ecopath catch data to gear parameters
+#' Sets up gear parameters from Ecopath catch data
+#'
+#' Currently this sets up only a single gear for the total catch. The
+#' `yield_observed` gear parameter is set to the total catch for each species.
+#' The `catchability` is set to the ratio of the observed yield to the observed
+#' biomass. The selectivity of the gear is assumed to be sigmoidal with 50% of
+#' fish selected at maturity size and 25% selected at 90% of maturity size
+#' (measured in weight). Fishing is turned on with an initial effort of 1. The
+#' result will therefore not be at steady state yet.
 #'
 #' @param params A MizerParams object
 #' @param ecopath_catch The Ecopath Catch data frame
@@ -18,10 +26,12 @@ addEcopathCatchTotal <- function(params, ecopath_catch) {
         gear = "total",
         sel_func = "sigmoid_length",
         l50 = w2l(sp$w_mat, params),
-        l25 = w2l(sp$w_mat, params) * 0.8,
-        catchability = 1,
+        l25 = w2l(sp$w_mat, params) * 0.9,
+        # catchability and yield_observed will be extracted from Ecopath later
+        catchability = 0,
         yield_observed = 0
     )
+    # Extract total yield for each species from Ecopath
     for (i in seq_len(nrow(sp))) {
         for (group in sp$ecopath_groups[[i]]) {
             yield <- ecopath_catch$TotalCatch..t.km..year.[ecopath_catch$Group.name == group]
@@ -31,8 +41,11 @@ addEcopathCatchTotal <- function(params, ecopath_catch) {
             gp$yield_observed[i] <- sum(gp$yield_observed[i], yield, na.rm = TRUE)
         }
     }
+    # Set catchability
     gp$catchability <- gp$yield_observed / sp$biomass_observed
-    gear_params(params) <- gp
+    # Set gear parameters
+    gear_params(params) <- validGearParams(gp, sp)
+    # Turn on fishing
     initial_effort(params) <- 1
 
     return(params)
