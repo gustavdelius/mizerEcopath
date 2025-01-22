dcb1e5aca044167d3e1bb7f654b6e46a8e05439d
Gustav Delius <gustav.delius@gmail.com>
2025-01-22 17:23:58 +0000

    Remove centering code from `matchDiet()`

--- R/matchDiet.R
+++ R/matchDiet.R
@@ -4,37 +4,15 @@
 #' matrix matches the Ecopath diet matrix. It then adjusts the external
 #' encounter and mortality rates so that the steady state is not changed.
 #'
-#' The function will raise an error if negative external encounter rates are
-#' required for any species and issues a warning for any species that requires
-#' negative external mortality rates.
-#'
-#' Note that the function does not currently work properly when the mizer model
-#' makes use of additional ecosystem components that add to the encounter rate.
-#'
-#' @section Centering:
-#' Ecopath diet matrices often contain many zeros, which would mean that it is
-#' impossible for two species to interact. This is because the Ecopath diet
-#' matrix does not account for small prey items that do not contribute
-#' significantly to a predator's diet. The `centering` parameter allows one to
-#' experiment with adding an estimate of the diet of these small prey to the
-#' diet matrix.
-#'
-#' For this purpose we assume that the Ecopath diet matrix did not account for
-#' prey items with a weight below `w_cutoff`. To estimate their contribution to
-#' the diet we make the assumption that for such small prey items the predator
-#' does not distinguish between species and feeds solely according to its prey
-#' size preference. We multiply the resulting encounter rate of these small
-#' prey items by the `centering` argument and add it to the Ecopath diet matrix
-#' before matching.
+#' The function will raise a warning for any species that requires a negative
+#' external mortality rate or negative external encounter rate.
 #'
 #' @param params A MizerParams object
 #' @param diet_matrix The Ecopath diet matrix as produced by
 #'   `reduceEcopathDiet()`.
-#' @param centering The centering parameter, see "Centering" section below for
-#'   details.
-#' @param w_prey_cutoff See "Centering" section below for details.
 #'
-#' @return A MizerParams object with the diet matrix matched.
+#' @return A MizerParams object with the interaction matrix set so that the
+#'   desired diet is achieved.
 #' @family match functions
 #' @export
 matchDiet <- function(params, diet_matrix, centering = 0,
@@ -44,11 +22,6 @@ matchDiet <- function(params, diet_matrix, centering = 0,
     }
     sp <- params@species_params
     no_sp <- nrow(sp)
-
-    # This function is to be used on a model with infinite maximum intake rate
-    if (any(intake_max(params) != Inf)) {
-        stop("This function is for models with infinite maximum intake rate.")
-    }
 
     # Check diet matrix ----
     if (!is(diet_matrix, "matrix")) {
@@ -87,46 +60,11 @@ matchDiet <- function(params, diet_matrix, centering = 0,
     # If the encounter matrix has zeros where the Ecopath diet does not, then
     # there is no way to match the diet matrix. This is unlikely to happen.
     if (any(E == 0 & D > 0)) {
-        stop("Diet matrix cannot be matched because the mizer predation kernel does lead to zero predator-prey interaction between some species.")
+        stop("Diet matrix cannot be matched because the mizer predation kernel leads to zero predator-prey interaction between some species.")
     }
 
     theta <- D / E
     theta[is.nan(theta)] <- 1  # Avoid NaNs when E is zero
-
-    params <- makeInteractive(params, theta)
-
-    if (centering == 0) {
-        return(params)
-    }
-    stop("Centering not implemented yet.")
-
-    # Diet matrix restricted to large prey
-    E <- getDietMatrix(p, min_w_prey = w_prey_cutoff)[, 1:no_sp]
-    # Diet matrix restricted to small prey
-    S <- getDietMatrix(p, max_w_prey = w_prey_cutoff)[, 1:no_sp]
-
-    # If more of the encounter comes from small prey than from large prey then
-    # we are more free to keep that entry of the interaction matrix close to 1
-    centering <- centering * S / (E + S)
-
-    # If the encounter matrix has zeros where the Ecopath diet does not, then
-    # there is no way to match the diet matrix. This is unlikely to happen.
-    if (any(E == 0 & D > 0)) {
-        stop("Diet matrix cannot be matched because the mizer predation kernel does lead to zero predator-prey interaction between some species.")
-    }
-
-    # We want to set the interaction matrix so that the diet E of non-larval
-    # prey in the mizer model is close to the ecopath diet D. In other we want
-    # the following ratios to be close to 1:
-    ratios <- D / E
-    ratios[is.nan(ratios)] <- 1  # Avoid NaNs when E is zero
-    # If at the same time we want to keep species preferences approximately
-    # equal we would use:
-    # theta <- (ratios + centering * rowMeans(ratios)) / (1 + centering)
-    # The problem with that is that when the ecopath diet matrix entry is zero
-    # then the corresponding entry in the interaction matrix will be zero.
-    # We will instead penalise deviations from 1 in theta
-    theta <- (ratios + centering) / (1 + centering)
 
     params <- makeInteractive(params, theta)
 
