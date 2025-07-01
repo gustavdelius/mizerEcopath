b990607f8ea7328bebd0cae392887f43931b6c8d
Jessica Westworth <jwestworth04@gmail.com>
2025-07-01 11:15:07 +0100

    Changes to the reutrn button. Now after calling finalise params, we add the command which sets the R_max value to Inf and the reproduction level to 0.

--- R/ecopath_tune.R
+++ R/ecopath_tune.R
@@ -455,7 +455,11 @@ tuneEcopath <- function(params, catch = NULL, diet = NULL,
         # return with the latest params object
         observeEvent(input$done, {
             file.remove(logs$files)
-            stopApp(finalise_params(params()))
+            p <- params()
+            p <- finalise_params(p)
+            p <- setBevertonHolt(p, R_max = Inf)
+            p <- setBevertonHolt(p, reproduction_level = 0)
+            stopApp(p)
         })
 
     } #the server
